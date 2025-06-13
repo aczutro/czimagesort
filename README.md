d0e0fc35a0e9b9edd236c611bab8eac09cbb8840
Alexander Czutro <github@czutro.ch>
2025-06-13 15:18:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# czimagesort
+
+Application to easily sort images into configurable categories.
+
+
+## Installation
+
+Change into  the root directory  of this distribution (where  the file
+`pyproject.toml` is) and run the following command:
+
+```shell
+pip install .
+```
+
+
+## Changelog
+
+### Version 1.0.0: first release
---
+++ src/czimagesort/czimagesort.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2021 - present  Alexander Czutro <github@czutro.ch>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# For more details, see the provided licence file or
+# <http://www.gnu.org/licenses>.
+#
+################################################################### aczutro ###
+
+"""
+Application to easily sort images into configurable categories.
+"""
+
+
+def main():
+    print("hello world")
+#main
+
+
+### aczutro ###################################################################
